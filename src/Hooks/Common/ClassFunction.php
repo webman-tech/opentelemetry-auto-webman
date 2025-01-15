b4168b3f3d33db8a896cf46baeeddc27521dde75
kriss <462679766@qq.com>
2025-01-15 15:23:20 +0800

    fix type error

--- src/Hooks/Common/ClassFunction.php
+++ src/Hooks/Common/ClassFunction.php
@@ -5,28 +5,29 @@ declare(strict_types=1);
 namespace OpenTelemetry\Contrib\Instrumentation\Webman\Hooks\Common;
 
 use OpenTelemetry\API\Instrumentation\CachedInstrumentation;
+use OpenTelemetry\API\Trace\SpanKind;
 use OpenTelemetry\Context\Context;
 use OpenTelemetry\Contrib\Instrumentation\Webman\Hooks\CommonHookTrait;
 use Throwable;
-use Webman\MiddlewareInterface;
 use function OpenTelemetry\Instrumentation\hook;
 
 final class ClassFunction
 {
     use CommonHookTrait;
 
-    public static function hook(CachedInstrumentation $instrumentation, string $clasName, string $functionName): void
+    public static function hook(CachedInstrumentation $instrumentation, string $clasName, string $functionName, int $kind = SpanKind::KIND_SERVER): void
     {
         hook(
             $clasName,
             $functionName,
-            pre: function (MiddlewareInterface $middleware, array $params, string $class, string $function, ?string $filename, ?int $lineno) use ($instrumentation) {
+            pre: function ($object, array $params, string $class, string $function, ?string $filename, ?int $lineno) use ($instrumentation, $kind) {
                 $span = self::builder($instrumentation, sprintf('%s::%s', $class, $function), $function, $class, $filename, $lineno)
+                    ->setSpanKind($kind)
                     ->startSpan();
 
                 Context::storage()->attach($span->storeInContext(Context::getCurrent()));
             },
-            post: function (MiddlewareInterface $middleware, array $params, $returnValue, ?Throwable $exception) {
+            post: function ($object, array $params, $returnValue, ?Throwable $exception) {
                 self::end($exception);
             }
         );
